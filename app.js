aaf2e6e9b029825cd112d322a5a01558a43d5b63
Timon van Spronsen <timonvanspronsen@outlook.com>
2016-10-26 21:47:35 +0200

    Add gif handler

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const irc = require('irc')
-const axios = require('axios')
+const fetch = require('node-fetch')
 
 const client = new irc.Client('bofh.nl.smurfnet.ch', 'Nodeschool', {
-    channels: ['#parrot', '#compare', '#weather']
+    channels: ['#parrot', '#compare', '#gifs']
 })
 
 client.addListener('message', (from, to, message) => {
@@ -27,9 +27,26 @@ client.addListener('message#compare', (from, message) => {
   }
 })
 
-client.addListener('message#weather', (from, message) => {
-  if (!message.startsWith('weather')) return
+client.addListener('message#gifs', (from, message) => {
+  const c = client.say.bind(client, '#gifs')
 
-  const [_, city] = message.split(' ')
-  console.log(city)
+  if (!message.startsWith('gif')) return
+
+  const [_, keywords] = message.split(' ')
+
+  if (!keywords) {
+    fetch('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC')
+      .then(res => res.json())
+      .then(({ data }) => {
+        c(data.image_url)
+      })
+  } else {
+    console.log('els')
+    fetch(`http://api.giphy.com/v1/gifs/search?q=${encodeURIComponent(keywords)}&api_key=dc6zaTOxFJmzC`)
+      .then(res => res.json())
+      .then(({ data }) => {
+        c(data[0].images.fixed_height.url)
+      })
+      .catch(err => console.log(err))
+  }
 })
